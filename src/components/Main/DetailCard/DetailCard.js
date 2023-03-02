5163444cfa672d66492f5e262e27c823f57a963d
ZManak <javifuer@gmail.com>
2023-03-02 20:47:54 +0100

    add/detailView

--- src/components/Main/DetailCard/DetailCard.js
+++ src/components/Main/DetailCard/DetailCard.js
@@ -2,10 +2,10 @@ import React, { useEffect, useState} from 'react';
 import { useLocation } from 'react-router-dom';
 
 export default function DetailCard(props) {
-  const [pokemon, setPokemon] = useState('')
+  const [pokemon, setPokemon] = useState()
   const [image, setImage] = useState({})
-  
-  
+
+
   const name = useLocation()
   const param = name.pathname.slice(9,)
 
@@ -22,18 +22,18 @@ export default function DetailCard(props) {
         alert(err)
       }}
       getDetail();
-    },    
+    },
      // eslint-disable-next-line react-hooks/exhaustive-deps
   [])
-  
+
   //if(arrNew === [undefined, undefined]) {
-    return (<article className='detailCard'>
+  return pokemon ? (<article className='detailCard'>
       <h1>PokeDetails</h1>
       <h3>{pokemon.name}</h3>
       <img src={image.front_default} alt={pokemon.name} />
       <p>ID: {pokemon.id}</p>
-      <p>Type 1: </p>
-      <p>Type 2: </p>
-    </article>  
-  ) 
+      <p>Type 1: {pokemon.types[0].type.name}</p>
+      <p>Type 2: {(pokemon.types.length > 1) ? pokemon.types[1].type.name : "none"}</p>
+    </article>
+  ) : <></>
 }
