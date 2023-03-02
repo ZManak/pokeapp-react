be26d8ca52df260202e3164577b964b666495a17
ZManak <javifuer@gmail.com>
2023-03-02 16:23:44 +0100

    deploy

--- src/components/Main/DetailCard/DetailCard.js
+++ src/components/Main/DetailCard/DetailCard.js
@@ -23,6 +23,7 @@ export default function DetailCard(props) {
       }}
       getDetail();
     },    
+     // eslint-disable-next-line react-hooks/exhaustive-deps
   [])
   
   //if(arrNew === [undefined, undefined]) {
